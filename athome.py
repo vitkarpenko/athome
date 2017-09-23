ee9ecf94efdf8db3465fa7c175b1931c58a48959
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-09-23 19:24:27 +0300

    Meh. My router still sucks. But I've deleted an empty line and it's makes me feel better.

--- athome.py
+++ athome.py
@@ -7,7 +7,6 @@ import requests
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
                   '(KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36',
-
 }
 
 DATA = {
