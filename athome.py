ecf24c21f35b656a02dd771829bcc21e9afa2641
Vitaly Karpenko <vitkarpenko@gmail.com>
2018-11-20 21:58:52 +0300

    Вроде работает

--- athome.py
+++ athome.py
@@ -1,35 +1,88 @@
 import os
-import time
+import random
+import telnetlib
+from time import sleep
 
-import requests
+import vk_api
 
+ROUTER_LOGIN = os.getenv('ROUTER_LOGIN').encode()
+ROUTER_PASSWORD = os.getenv('ROUTER_PASSWORD').encode()
 
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36',
+VK_LOGIN = os.getenv('VK_LOGIN')
+VK_PASSWORD = os.getenv('VK_PASSWORD')
+
+MOM_VK_ID = 187_822_398
+
+NAMES_TO_MAC_AND_CUTIES = {
+    'vitaly': ('f4:f5:db:c8:09:b1', ['Виталик']),
+    'veva': (
+        'f0:76:6f:a2:38:ef',
+        [
+            'Биба',
+            'Капуста',
+            'Суслик',
+            'Капуста',
+            'Батутик',
+            'Ушастик',
+            'Припузинка',
+            'Сплюшка',
+            'Тутурушка',
+            'Вева',
+            'Верочка',
+        ],
+    ),
 }
 
-DATA = {
-    'username': os.environ['ATHOME_LOGIN'],
-    'password': os.environ['ATHOME_PASSWORD'],
-    'submit.htm?login.htm': 'Send'
-}
+
+def telnet_login():
+    telnet = telnetlib.Telnet('192.168.1.1')
+    telnet.read_until(b'Login:')
+    telnet.write(ROUTER_LOGIN + b"\n")
+    telnet.read_until(b'Password:')
+    telnet.write(ROUTER_PASSWORD + b"\n")
+    return telnet
 
 
-def auth(session, headers, data):
-    response = session.post('http://192.168.0.1/login.cgi', headers=headers, data=data)
-    return response
+def vk_login():
+    vk_session = vk_api.VkApi(VK_LOGIN, VK_PASSWORD)
+    vk_session.auth()
+    return vk_session.get_api()
 
 
-while True:
-    try:
-        with requests.Session() as s:
-            auth(s, headers=HEADERS, data=DATA)
-    except requests.exceptions.ConnectionError:
-        continue
-    else:
-        break
+def show_associations(telnet):
+    telnet.write(b"show associations\n")
+    sleep(1)
+    return telnet.read_very_eager().decode()
 
-response = s.get('http://192.168.0.1/wlstatbl.htm')
-print(response.text)
 
+def send_notification(vk, name, old_state, new_state):
+    cutie = random.choice(NAMES_TO_MAC_AND_CUTIES[name][1])
+
+    if old_state == new_state:
+        return
+
+    if old_state == False and new_state == True:
+        message = f'{cutie} вернулся домой :)'
+    elif old_state == True and new_state == False:
+        message = f'{cutie} куда-то попёрся...'
+    elif old_state == None:
+        message = f'{cutie} дома.' if new_state else f'{cutie} где-то шлындает.'
+    vk.messages.send(message=message, user_id=MOM_VK_ID)
+
+
+def main():
+    vk = vk_login()
+    telnet = telnet_login()
+    athome = {name: None for name in NAMES_TO_MAC_AND_CUTIES}
+    while True:
+        associations = show_associations(telnet)
+        for name in NAMES_TO_MAC_AND_CUTIES:
+            old_state = athome[name]
+            new_state = NAMES_TO_MAC_AND_CUTIES[name][0] in associations
+            send_notification(vk, name, old_state, new_state)
+            athome[name] = new_state
+        sleep(5)
+
+
+if __name__ == "__main__":
+    main()
